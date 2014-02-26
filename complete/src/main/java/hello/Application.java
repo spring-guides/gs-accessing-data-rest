250a6ffd6ebf09b85b10e241d51ac26da77d1075
Greg Turnquist <gturnquist@gopivotal.com>
2014-02-26 16:01:29 -0500

    Write the guide

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -14,6 +14,6 @@ import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguratio
 public class Application {
 
 	public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+		SpringApplication.run(Application.class, args);
+	}
 }
