51d7a2104030ca38a9550e5dee697fa1f90751ae
Greg Turnquist <gturnquist@gopivotal.com>
2014-02-26 12:32:16 -0500

    Remove extraneous bits

--- complete/src/main/java/hello/Person.java
+++ complete/src/main/java/hello/Person.java
@@ -15,7 +15,6 @@ public class Person {
 	private String firstName;
 	private String lastName;
 
-
 	public String getFirstName() {
 		return firstName;
 	}
--- complete/src/main/java/hello/PersonRepository.java
+++ complete/src/main/java/hello/PersonRepository.java
@@ -1,8 +1,6 @@
 package hello;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "people", path = "people")
