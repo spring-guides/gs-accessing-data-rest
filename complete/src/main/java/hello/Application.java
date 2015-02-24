5729adeca99d290845ba7c7a84d6a7eb35d45d01
Greg Turnquist <gturnquist@gopivotal.com>
2015-02-24 13:52:00 -0600

    Upgrade to @SpringBootApplication

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -1,16 +1,9 @@
 package hello;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Configuration
-@EnableJpaRepositories
-@Import(RepositoryRestMvcConfiguration.class)
-@EnableAutoConfiguration
+@SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
