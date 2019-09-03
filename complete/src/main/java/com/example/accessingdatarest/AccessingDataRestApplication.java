ad44bdab62886d74a872c4ffcd4207ab795fa4eb
Jay Bryant <jbryant@pivotal.io>
2019-09-03 13:24:43 -0500

    Use the Spring Initializr for setup
    I rewrote the readme and the code (just had to change packages and the application name) to use the Spring Initializr and the stub project it generates.

--- complete/src/main/java/com/example/accessingdatarest/AccessingDataRestApplication.java
+++ complete/src/main/java/com/example/accessingdatarest/AccessingDataRestApplication.java
@@ -1,12 +1,12 @@
-package hello;
+package com.example.accessingdatarest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class AccessingDataRestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(AccessingDataRestApplication.class, args);
 	}
 }
