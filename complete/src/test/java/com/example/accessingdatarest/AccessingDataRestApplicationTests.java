5ab72a41f7cfae9c8daa67e64fff8a993d12746d
Jay Bryant <jbryant@pivotal.io>
2019-10-22 09:32:46 -0500

    Removing unused imports
    Sam Brannen noticed that I had failed to remove an import, and I found another unused import. I removed both of them.

--- complete/src/test/java/com/example/accessingdatarest/AccessingDataRestApplicationTests.java
+++ complete/src/test/java/com/example/accessingdatarest/AccessingDataRestApplicationTests.java
@@ -18,12 +18,10 @@ package com.example.accessingdatarest;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
