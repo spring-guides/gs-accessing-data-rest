f83bdff2233be1225eafac9abd3058160f61b6c9
Jay Bryant <jbryant@vmware.com>
2022-11-29 10:42:34 -0600

    Upgraded to Spring Boot 3.0.0
    to get on the new major version of Spring Boot.

--- complete/src/main/java/com/example/accessingdatarest/PersonRepository.java
+++ complete/src/main/java/com/example/accessingdatarest/PersonRepository.java
@@ -3,11 +3,12 @@ package com.example.accessingdatarest;
 import java.util.List;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "people", path = "people")
-public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
+public interface PersonRepository extends PagingAndSortingRepository<Person, Long>, CrudRepository<Person,Long> {
 
 	List<Person> findByLastName(@Param("name") String name);
 
