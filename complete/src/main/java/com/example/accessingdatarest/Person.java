d8f6dbaae83ca63539d59f95dfa6a1c2214bf246
George Panaretos <georgepanaretos.cac@gmail.com>
2023-01-03 16:39:10 +0100

    Clean up Person.java imports
    Due to there is not explanation for the Annotations in this guide I believe we can merge the imports into a singular one.

--- complete/src/main/java/com/example/accessingdatarest/Person.java
+++ complete/src/main/java/com/example/accessingdatarest/Person.java
@@ -1,9 +1,6 @@
 package com.example.accessingdatarest;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Person {
